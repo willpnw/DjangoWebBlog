ef3d3c6785207e21ef20730e957385ad2afce9f7
will <patterson.wj@gmail.com>
2014-09-14 12:03:25 -0700

    Web Blog

---
+++ netmag/urls.py
@@ -0,0 +1,8 @@
+from django.conf.urls import patterns, include, url
+from django.contrib import admin
+
+urlpatterns = patterns('',
+	url(r'^admin/', include(admin.site.urls)),
+	url(r'^$', 'blog.views.index'),
+	url(r'^(?P<slug>[\w\-]+)/$', 'blog.views.post')
+)
